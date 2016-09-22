949de8cf12d9a1dc4f6ecc3762b0a9594a36ca15
Ford-9 <Ford-9@mail.ru>
2016-09-23 02:45:47 +0300

    Create template for HTML pages.

---
+++ template.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class=" col-lg-6 text-right">
+            <h1><strong>Маккаби</strong><br><small>Израиль</small></h1>
+            <h1>3</h1>
+        </div>
+        <div class=" col-lg-6 text-left">
+            <h1><strong>Зенит</strong><br><small>Россия</small></h1>
+            <h1>4</h1>
+        </div>
+    </div>
+
+
+<!---    ===Краткое описание важных событий===   -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h4>Краткое описание важных событий</h4>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Время</th>
+                    <th>Событие</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>90</td>
+                    <td>\"Зенит\", выступавший в последние годы в Лиге чемпионов, является фаворитом группы. \"Маккаби\" Тель-Авив по подбору исполнителей не может сравниться со своим соперникам, и шансов на успех даже в родных стенах у жёлтых дьяволов немного. Классом россияне однозначно выше, да и оборона \"Маккаби\" видится слишком уж уступчивой, пропуская большое количество мячей даже в слабом по составу местном чемпионате. Сегодня вполне можно рассчитывать на довольно интересный поединок.</td>
+                </tr>
+                <tr>
+                    <td>33</td>
+                    <td>\"Зенит\", выступавший в последние годы в Лиге чемпионов, является фаворитом группы. \"Маккаби\" Тель-Авив по подбору исполнителей не может сравниться со своим соперникам, и шансов на успех даже в родных стенах у жёлтых дьяволов немного. Классом россияне однозначно выше, да и оборона \"Маккаби\" видится слишком уж уступчивой, пропуская большое количество мячей даже в слабом по составу местном чемпионате. Сегодня вполне можно рассчитывать на довольно интересный поединок.</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+<!--    ===Список игроков===    -->
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h3>Список игроков</h3>
+        </div>
+        <div class=" col-lg-6">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Игрок</th>
+                    <th>Время на поле</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><h4>Список основных игроков</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><h4>Список игроков, которые вышли на замену</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><h4>Список запасных игроков</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class=" col-lg-6">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Игрок</th>
+                    <th>Время на поле</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><h4>Список основных игроков</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><h4>Список игроков, которые вышли на замену</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><h4>Список запасных игроков</h4></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>222</td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
